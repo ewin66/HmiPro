e3857ad539d3fa8d95c400bb5a32e3ccd4b59186
ychost <ychost@outlook.com>
2018-02-03 13:22:27 +0800

    fix: 参数界面不能显示485状态

---
+++ HmiPro/Converts/StringFormatConvert.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace HmiPro.Converts {
+    /// <summary>
+    /// 解决 Wpf 自带的 StringFormat 不支持对象为字符串
+    /// <author>ychost</author>
+    /// <date>2018-2-3</date>
+    /// </summary>
+    public class StringFormatConvert : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            if (value is string) {
+                return value;
+            } else if (double.TryParse(value?.ToString(), out var dbValue)) {
+                if (parameter is string fmt) {
+                    return dbValue.ToString(fmt);
+                }
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
